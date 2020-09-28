58e967a0da630a35781b64d15a5e7ad76a0c6c9e
amathur267 <71085445+amathur267@users.noreply.github.com>
2020-09-28 17:03:17 +0530

    first commit

---
+++ greenscreen/README.markdown
@@ -0,0 +1,5 @@
+# GreenScreen
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/amathur267/pen/mdPLazx](https://codepen.io/amathur267/pen/mdPLazx).
+
+
---
+++ greenscreen/dist/script.js
@@ -0,0 +1,61 @@
+var fgImage = null;
+var bgImage = null;
+var fgCanvas;
+var bgCanvas;
+
+function loadForegroundImage() {
+  var file = document.getElementById("fgfile");
+  fgImage = new SimpleImage(file);
+  fgCanvas = document.getElementById("fgcan");
+  fgImage.drawTo(fgCanvas);
+}
+
+function loadBackgroundImage() {
+  var file = document.getElementById("bgfile");
+  bgImage = new SimpleImage(file);
+  bgCanvas = document.getElementById("bgcan");
+  bgImage.drawTo(bgCanvas);
+}
+
+function createComposite() {
+  var output = new SimpleImage(fgImage.getWidth(),fgImage.getHeight());
+  var greenThreshold = 230;
+  for (var pixel of fgImage.values()) {
+    var x = pixel.getX();
+    var y = pixel.getY();
+    if (pixel.getRed()+pixel.getBlue()<pixel.getGreen()) {
+      var bgPixel = bgImage.getPixel(x,y);
+      output.setPixel(x,y,bgPixel);
+    }
+    else {
+      //pixel is not green, use foreground
+      output.setPixel(x,y,pixel);
+    }
+  }
+  return output;
+}
+
+function doGreenScreen() {
+  //check that images are loaded
+  if (fgImage == null  || ! fgImage.complete()) {
+    alert("Foreground image not loaded");
+  }
+  if (bgImage == null || ! bgImage.complete()) {
+    alert("Background image not loaded");
+  }
+  // clear canvases
+  clearCanvas();
+  // call createComposite, which does green screen algorithm and returns a composite image
+  var finalImage = createComposite();
+  finalImage.drawTo(fgCanvas);
+}
+
+function clearCanvas() {
+  doClear(fgCanvas);
+  doClear(bgCanvas);
+}
+
+function doClear(canvas) {
+  var context = canvas.getContext("2d");
+  context.clearRect(0,0,canvas.width,canvas.height);
+}
